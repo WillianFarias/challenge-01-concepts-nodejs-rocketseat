260df08aac46ee8a292b1cc426f11dd77e8ba71f
WillianFarias <williansousafarias@gmail.com>
2021-05-16 16:30:41 -0300

    post user and checkExistUserAccount

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { v4: uuid } = require('uuid');
 
 // const { v4: uuidv4 } = require('uuid');
 
@@ -8,14 +9,37 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if(!user) {
+    return response.status(400).json({ error: 'User already registered! '});
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+
+  const { username } = request.headers;
+  const { name } = request.body;
+
+  const checksExistsUserAccount = users.some(user => user.username === username);
+
+  if (checksExistsUserAccount) {
+    return response.status(400).json({ error: 'User already registered' });
+  }
+
+  const user = {id: uuid(), name, username, todos:[]}
+  users.push(user);
+
+  return response.status(201).json(user);
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
