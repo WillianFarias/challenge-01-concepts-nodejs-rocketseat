269f8ef393e64d9410a3781a1f7f148e06c3fddd
WillianFarias <williansousafarias@gmail.com>
2021-05-17 19:16:31 -0300

    post todos

--- src/index.js
+++ src/index.js
@@ -51,7 +51,21 @@ app.get('/todos', checksExistsUserAccount, (request, response) => {
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { title, deadline } = request.body;
+  const { username } = request.headers;
+
+  const todo = {
+    id: uuid(),
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date()
+  }
+
+  const userIndex = users.findIndex(user => user.username === username);
+  users[userIndex].todos.push(todo);
+
+  return response.status(201).json(todo);
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
