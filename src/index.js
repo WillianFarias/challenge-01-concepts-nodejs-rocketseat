1d5c6d9f88f651c729ee6ac7186fd305ff59fee9
WillianFarias <williansousafarias@gmail.com>
2021-05-17 07:43:20 -0300

    get todos

--- src/index.js
+++ src/index.js
@@ -43,7 +43,11 @@ app.post('/users', (request, response) => {
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username == username);
+
+  return response.status(200).json(user.todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
