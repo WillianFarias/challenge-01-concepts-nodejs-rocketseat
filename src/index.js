b74a4380ee792eecfc95798a50177997da6b6819
WillianFarias <williansousafarias@gmail.com>
2021-05-19 20:43:08 -0300

    put /todos/:id

--- src/index.js
+++ src/index.js
@@ -69,7 +69,20 @@ app.post('/todos', checksExistsUserAccount, (request, response) => {
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { title, deadline } = request.body;
+  const { id } = request.query;
+
+  const { user } = request;
+  const indexTodo = user.todos.findIndex(todo => todo.id === id);
+
+  if (indexTodo < 0) {
+    return response.status(400).json({ error: 'Todo not found!' });
+  }
+  
+  user.todos[indexTodo].title = title;
+  user.todos[indexTodo].deadline = deadline;
+
+  return response.status(200).json(user);
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
